ac0a9d843c4640f282ed2fe91ee8e257df6d72f6
hannus.max-mb25 <hannus.max-mb25@mb.dhbw-ravensburg.de>
2026-03-09 18:37:28 +0100

    Structures hinzufügen

---
+++ ProjektabgabeTeil2/Program.vb
@@ -0,0 +1,13 @@
+Option Strict On
+Module Program
+    Structure UserRecord
+        Dim Id As String
+        Dim Name As String
+    End Structure
+    Structure BookRecord
+        Dim Isbn As String
+        Dim Title As String
+        Dim Author As String
+        Dim Status As String
+        Dim BorrowerId As String
+    End Structure
